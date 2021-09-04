6b64ea1bcd025762f2721b3b4fd7f83aa16162a0
Jonathan <jonathanwcj413@outlook.com>
2021-09-03 18:31:06 -0600

    nim not playing right

--- Nim/Nim.py
+++ Nim/Nim.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-print("Hello World")
-=======
-import time
+from time import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-def time():
-    return time.time()
 
 def Win(stones):
     '''
@@ -26,9 +21,9 @@ def Move(stones):
         return 1
     if stones == 2:
         return 1
-    if Win(stones-1):
+    if not Win(stones-1):
         return 1
-    if Win(stones-2):
+    if not Win(stones-2):
         return 2
 
 for i in range(1, 200):
@@ -37,15 +32,8 @@ for i in range(1, 200):
     while stones > 0:
         n = Move(stones)
         stones -= n
-        # print(stones)
+        print(stones)
     print('{}: {}'.format(i, time() - start))
 
 
-<<<<<<< HEAD
-# if player1:
-#     print("Player 1 wins!")
-# else:
-#     print("Player 2 wins!")
->>>>>>> 2c3c3cde155cbd133b7c70090842dbd4d3db7c6a
-=======
->>>>>>> ea4e4d0d7c74f6b3c862fc582f35d12fd90604ee
+
