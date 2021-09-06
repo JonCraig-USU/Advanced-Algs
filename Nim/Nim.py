b78b5e71cd258dab1c5f7c16ce4c5e3c8205bcf9
Jonathan <jonathanwcj413@outlook.com>
2021-09-05 19:18:07 -0600

    final draft

--- Nim/Nim.py
+++ Nim/Nim.py
@@ -2,7 +2,7 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# Recursive Functions
 def Win(stones):
     '''
     @Returns boolean for win condition based on if opponent will lose
@@ -27,14 +27,25 @@ def Move(stones):
         return 2
     return 2
 
+# Memoizing version of win condition
+def WinMem(stones):
+    # alocate memory
+    sol = [False] * (stones + 1)
+    #fill simple prob/solution
+    sol[0] = True
+    # fill all solutions
+    for j in range(2, stones + 1):
+        sol[j] = not(sol[j-1] and sol[j - 2])
+    return sol[stones]
 
-
+# set variables and arrays for the recursive solution
 xValues = []
 x = 0
 times = []
 begin = time.time()
 
-while (time.time() - begin) < 600:
+# While loop for time controlled ending
+while (time.time() - begin) < 1200:
     stones = x
     start = time.time()
     while stones > 0:
@@ -44,13 +55,20 @@ while (time.time() - begin) < 600:
     times.append(time.time() - start)
     xValues.append(x)
     x += 1
+print("Time: " + str(time.time() - begin))
 
+# variables and calculation for best fit line
 bestX = np.asanyarray(xValues)
 bestY = np.asanyarray(times)
 m, b = np.polyfit(bestX, bestY, 1)
 
+# calculate the best fit line y values
+lineY = []
+for entry in bestX:
+    lineY.append(m * entry + b)
+
 # set the graph info
-plt.plot(xValues, times, 'ro', xValues, m*xValues+b, '-')
+plt.plot(xValues, times, 'ro', xValues, lineY, '-')
 plt.yscale('log')
 
 # label the graph and axes
@@ -60,6 +78,16 @@ plt.ylabel("Time")
 
 # draw the graph
 plt.show()
+print("Slope: " + str(m))
+
+# Check that the solutions match
+for k in range(0, 35):
+    norm = Win(k)
+    mem = WinMem(k)
+    if mem != norm:
+        print('Got another error Captain')
+        print('{}: {}'.format(norm, mem))
+
 
 
 
--- Nim/README.md
+++ Nim/README.md
@@ -1,1 +1,8 @@
-Advanced Algs
+# Advanced Algs
+## Highest calculated number was 59 stones
+Longest I ever ran it was 15 minutes, my patience ran out after that
+
+## Slope
+Never got very close to the 1.618  
+Shouldn't it have been an exponential line?  
+With 1.618 as the base?
